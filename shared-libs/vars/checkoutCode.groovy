0e9b095fad8d5686546dc4e76344a32c77e106b4
hicham farhi <hicham.farhi@hps-worldwide.com>
2024-02-27 16:51:15 +0100

    check again

--- shared-libs/vars/checkoutCode.groovy
+++ shared-libs/vars/checkoutCode.groovy
@@ -1,8 +1,7 @@
 def call(String repoUrl, String branch){
    def workingDir = "${env.WORKSPACE}"
    sh """  
-   if [ -d test-shared-libs ]
-   then
+   if [ -d 'test-shared-libs' ]; then
      pwd && ls
      rm -rf test-shared-libs && git clone ${repoUrl} ${workingDir}
    else
