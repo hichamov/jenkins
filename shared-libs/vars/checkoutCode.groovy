206a764be7de45fd40c3f30a55213facc3c03f0c
hicham farhi <hicham.farhi@hps-worldwide.com>
2024-02-27 16:55:03 +0100

    cheeck

--- shared-libs/vars/checkoutCode.groovy
+++ shared-libs/vars/checkoutCode.groovy
@@ -2,9 +2,11 @@ def call(String repoUrl, String branch){
    def workingDir = "${env.WORKSPACE}"
    sh '''  
    if [ -d 'test-shared-libs' ]; then
+     echo "dir exists"
      pwd && ls
      rm -rf test-shared-libs && git clone "${repoUrl}" "${workingDir}"
    else
+      echo "dir does not exist"
       git clone ${repoUrl} ${workingDir}
    fi
    '''
